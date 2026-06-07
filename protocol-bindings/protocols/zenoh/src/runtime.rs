5c1def0abe755da45ea3d36170f8449b472ca21b
yushun <yushun1990@gmail.com>
2026-06-07 15:13:53 +0800

    Normalize zenoh-pico runtime module name

--- protocol-bindings/protocols/zenoh/src/runtime.rs
+++ protocol-bindings/protocols/zenoh/src/runtime.rs
@@ -1,7 +1,6 @@
 #[cfg(feature = "runtime-zenoh")]
 pub mod zenoh;
 #[cfg(feature = "runtime-zenoh-pico")]
-#[path = "runtime/zenoh-pico.rs"]
 pub mod zenoh_pico;
 
 #[cfg(feature = "runtime-zenoh")]
